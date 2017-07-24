973f1ed1339055d1ec5b7c45570c3c4ceb8b7e48
Sotirios Karanasios <karanaso@gmail.com>
2017-07-24 21:03:50 +0100

    Fix FiveMinutesAgo slow execution test

--- tests/unit/__tests__/unit/components/MinutesAgo/MinutesAgo.test.jsx
+++ tests/unit/__tests__/unit/components/MinutesAgo/MinutesAgo.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import moment from 'moment';
+import timekeeper from 'timekeeper';
 
 import MinutesAgo from '../../../../../../source/components/MinutesAgo/MinutesAgo';
 
@@ -16,10 +17,14 @@ describe('<MinutesAgo/>',  () => {
   });
 
   it('should have text="5 minutes ago"', () => {
-    const time = moment().subtract(5,'minutes').format('DD/MM/YYYY HH:mm');
-    console.log(time);
+
+    timekeeper.freeze(moment().toISOString());
+    const now = moment();
+
+    const fiveMinutesAgo = now.subtract(5,'minutes').format('DD/MM/YYYY HH:mm');
     const expectResult = '5 minutes ago';
-    const wrapper = shallow(<MinutesAgo created={time}/>);
+
+    const wrapper = shallow(<MinutesAgo created={fiveMinutesAgo}/>);
     expect(wrapper.find('div').text()).toEqual(expectResult);
   });
 });
